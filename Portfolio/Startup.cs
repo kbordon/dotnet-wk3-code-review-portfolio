c797ad7147e18a0fae594bbaa75820c00285689f
Kimberly Bordon <kbordon@gmail.com>
2018-01-19 13:47:59 -0800

    Fix Startup.

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -52,6 +52,7 @@ namespace Portfolio
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseIdentity();
 
             app.UseMvc(routes =>
             {
