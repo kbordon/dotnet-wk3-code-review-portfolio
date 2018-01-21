22f91a4ddcf14dcce05e696a746620e91a9312d6
Kimberly Bordon <kbordon@gmail.com>
2018-01-20 18:04:48 -0800

    Create Review model.

---
+++ Portfolio/Controllers/ReviewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly AppDbContext _db;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Portfolio/Models/Review.cs
+++ Portfolio/Models/Review.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace Portfolio.Models
 {
-    [Table("posts")]
-    public class Post
+    [Table("reviews")]
+    public class Review
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Date { get; set; }
-        public virtual BlogUser User { get; set; }
-        public virtual ICollection<Review> Reviews { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
 
-        public Post()
+        public Review()
         {
             Date = DateTime.Now;
         }
