aca974a7de48dc500e09d532ea46792b2627eceb
Kimberly Bordon <kbordon@gmail.com>
2018-01-23 20:26:10 -0800

    Allow roles to be deleted.

--- Portfolio/Controllers/RoleController.cs
+++ Portfolio/Controllers/RoleController.cs
@@ -56,14 +56,45 @@ namespace Portfolio.Controllers
 
         public IActionResult Manage()
         {
-            ManageRole manager = new ManageRole
-            {
-                BlogRoles = _db.Roles.ToList()
-            };
-            manager.SetSelectList();
-            return View(manager);
+            ViewBag.RoleList = new SelectList(_db.Roles, "Name", "Name");
+            return View();
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRoleToUser(string UserName, string RoleName)
+        {
+            var user = _db.Users.Where(u => u.UserName == UserName).FirstOrDefault();
+            await _userManager.AddToRoleAsync(user, RoleName);
+            return RedirectToAction("Manage");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GetRoles(string UserName)
+        {
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                BlogUser user = _db.Users.Where(u => u.UserName == UserName).FirstOrDefault();
+
+                ViewBag.RolesForThisUser = await _userManager.GetRolesAsync(user);
+            }
+            ViewBag.RoleList = new SelectList(_db.Roles, "Name", "Name");
+            return View("Manage");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUserRole(string UserName, string RoleName)
+        {
+            BlogUser user = _db.Users.Where(u => u.UserName == UserName).FirstOrDefault();
+            if (await _userManager.IsInRoleAsync(user, RoleName))
+            {
+                await _userManager.RemoveFromRoleAsync(user, RoleName);
+            }
+
+            ViewBag.RoleList = new SelectList(_db.Roles, "Name", "Name");
+            return View("Manage");
+        }
     }
 }
