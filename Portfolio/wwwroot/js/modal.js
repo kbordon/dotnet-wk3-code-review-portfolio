8611cc413e2d1275fbb5c60f6cd8ab4a870895e4
Kimberly Bordon <kbordon@gmail.com>
2018-01-25 09:46:26 -0800

    Add exit functionality to crud modals of posts.

---
+++ Portfolio/wwwroot/js/modal.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+// exit modal
+    $('.exit').click(function(){
+        if($('#form').length)
+        {
+            $('#form').css("display", "none"); 
+        }
+        $(this).parents('div.comment-form').css("display", "none"); 
+    });
+})
+ 
